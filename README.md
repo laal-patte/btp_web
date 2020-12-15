0720fc8456cdc7409aaef628e23290254f218edd
siddharth jain <siddharth.jain@students.iiit.ac.in>
2020-12-15 22:37:24 +0530

    final check

---
+++ README.md
@@ -0,0 +1,10 @@
+# BTP-2 (Web Application)
+
+## Setup
+
+1. `apt-get install -y python python-setuptools python-dev build-essential python-pip`
+2. `pip install -r requirements.txt`
+
+## Running the app
+
+`python btp_flask.py` to start the server on `http://localhost:8080` 
---
+++ btp_flask.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+# Read all data into global dataframe
+DF = pd.read_csv('static/final.csv')
+DF['STATE'] = DF['STATE'].str.upper()
+DF['STATION CODE'] = DF['STATION CODE'].astype('int32')
+DF['WQI'] = DF['WQI'].round(3)
+
+
+@app.route('/', methods = ['GET'])
+def main():
+    return render_template('index.html')
+
+
+@app.route('/wqi', methods=['GET'])
+def wqi():
+    states = DF['STATE'].unique()
+    return render_template('wqi.html', states=states)
+
+@app.route('/wqi-r', methods=['GET'])
+def wqir():
+    rivers = DF['RIVER'].unique()
+    return render_template('wqir.html', rivers=rivers)
+
+@app.route('/stations/<state>', methods=['GET'])
+def get_stations(state):
+    stations = DF.loc[DF['STATE'] == state, ['STATION CODE', 'LOCATIONS']].sort_values(by=['STATION CODE'])
+    stations.reset_index(inplace=True, drop=True)
+    return jsonify({'stations': stations.to_dict('index')})
+
+@app.route('/stations-r/<river>', methods=['GET'])
+def get_stationsr(river):
+    stations = DF.loc[DF['RIVER'] == river, ['STATION CODE', 'LOCATIONS']].sort_values(by=['STATION CODE'])
+    stations.reset_index(inplace=True, drop=True)
+    return jsonify({'stations': stations.to_dict('index')})
+
+@app.route('/wqi/<station_code>', methods=['GET'])
+def get_wqi(station_code):
+    wqi = DF.loc[DF['STATION CODE'] == int(station_code), 'WQI'].values[0]
+    return jsonify({"WQI": wqi})
+
+@app.route('/wqi-r/<station_code>', methods=['GET'])
+def get_wqir(station_code):
+    wqi = DF.loc[DF['STATION CODE'] == int(station_code), 'WQI'].values[0]
+    return jsonify({"WQI": wqi})
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
